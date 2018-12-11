bee2c264240d2dd3a925825024bd5b17b2c86721
Mayja Tom <maja.morales17@gmail.com>
2018-12-10 20:40:09 -0800

    completed Spotify API calls

--- liri.js
+++ liri.js
@@ -1,8 +1,10 @@
 //grab spotify keys
 require("dotenv").config();
 
-//process.env.<the name of element> 
-console.log(process.env.SPOTIFY_ID)
+//adding required dependents
+var moment = require('moment');
+var request = require("request");
+var inquirer = require("inquirer");
 
 //import code not working. Commented it out
 // import {spotify} from "./keys.js";
@@ -14,19 +16,20 @@ var secretKeys = require("./keys.js");
 
 var Spotify = require('node-spotify-api');
 //did npm install node-spotify-api but don't understand the response
- 
+
 var spotify = new Spotify({
-  id: secretKeys.spotify.id,
-  secret: secretKeys.spotify.secret
+    id: secretKeys.spotify.id,
+    secret: secretKeys.spotify.secret
 });
- 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- 
-console.log(data); 
-});
+
+//lines 24-33 were to test my spotify api call. 
+// spotify.search({ type: 'track', query: song }, function(err, data) {
+//   if (err) {
+//     return console.log('Error occurred: ' + err);
+//   }
+
+// console.log(data.tracks.items); 
+// });
 
 //moment.js for date format from spotify moment("12-25-1995", "MM-DD-YYYY");
 // var moment = require('moment');
@@ -38,20 +41,45 @@ console.log(data);
 
 //4 commands- use Switch Case
 //concert-this <artists name> using bands in town api
-    //"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-        //returns
-        //name of venue
-        //venue location
-        //date of event using moment MM/DD/YYY
+//"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+//returns
+//name of venue
+//venue location
+//date of event using moment MM/DD/YYY
 
-//spotify-this-song "<song name here>" using node-spotify-api
+//spotify-this-song "<song name here>" using node-spotify-api. Using prompt to get user input, and switch function
+function searchSpotify() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "song",
+            message: "Type in a song name"
+        }
+    ]).then(function (userInput) {
+        var song = (userInput.song);
+        spotify.search({ type: 'track', query: song }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            } else {
+                console.log("Artists: " + data.tracks.items[0].artists[0].name);
+                console.log("Album: " + data.tracks.items[0].album.name);
+                console.log("Preview: " + data.tracks.items[0].album.href);
+                console.log("Song: " + userInput.song);
+            }
+        
+          });
+    });
+
+}
+searchSpotify();
+
     //returns
     //Artist(s)
     //Song Name
     //preview link of the song
     //album the song is from
     //if no song provided, default "The Sign" Ace of Base
- 
+
 //movie-this "<movie name here" using in class "trilogy" api Key
     //var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
         //returns
